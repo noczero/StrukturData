d6ac4fc3e1a7924d00ad95f1ffc2af0a1f6a9f05
noczero <zeroonetm@gmail.com>
2016-09-18 15:57:02 +0700

    Modul 3
    LPP

---
+++ Modul03/carwash.h
@@ -0,0 +1,44 @@
+#ifndef CARWASH_H_INCLUDED
+#define CARWASH_H_INCLUDED
+
+#define nil NULL
+#define info(p) (p) -> info
+#define next(p) (p) -> next
+#define first(l) ((l).first)
+
+
+
+struct infoType {
+	char noPol[30];
+	char merkMobil[30];
+};
+
+typedef struct elmList *address;
+
+struct elmList {	
+	infoType info;
+	address next;
+};
+
+struct list {
+	address first;
+};
+
+void createList(list &l);
+void dealokasi(address p);
+address alokasi(infoType x);
+
+void insertFirst(list &l , address p);
+void insertLast(list &l , address p);
+void insertAfter(address q , address p);
+
+void deleteFirst(list &l , address p);
+void deleteLast(list &l , address p);
+void deleteAfter(address q , address &p);
+
+address searchData(list l , infoType x);
+address searchDataBefore(list l , infoType x);
+void updateData(address p , infoType y);
+void printInfo(list l);
+
+#endif
---
+++ Modul03/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "carwash.h"
+
+using namespace std;
+
+int main(){
+	infoType Mobil;
+	list l;
+	createList(l);
+	strcpy(Mobil.noPol, "D 123 EF");
+	strcpy(Mobil.merkMobil, "Toyota");
+
+	address p;
+
+	p = alokasi(Mobil);
+	insertFirst(l , p);
+	printInfo(l);
+	//insertAfter(l , p);
+	return 0;
+}
